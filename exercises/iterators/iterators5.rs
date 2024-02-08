afe9dfd8c7eb407e5bb051f872188c553a442e8f
Mike <mikevanverseveld@hotmail.com>
2024-02-08 22:36:15 +0100

    Progress rustlings

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -35,7 +35,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.iter().fold(0, |v, (_, x)| { if x == &value {v + 1 } else {v}})
+    
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
